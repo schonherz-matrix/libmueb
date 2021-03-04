6a9f3d29f451b8f266e744fa63e1ecb2d0fd241c
Zsombor Bodnár <zsombor.bodnar@kszk.bme.hu>
2021-03-04 19:28:09 +0100

    WIP add manual tests to check compatibility

---
+++ tests/testmuebreceiver.cc
@@ -0,0 +1,34 @@
+#include <QApplication>
+#include <QLabel>
+#include <QTimer>
+
+#include "muebreceiver.h"
+
+class Task : public QObject {
+  Q_OBJECT
+
+ public:
+  Task(QObject *parent = 0)
+      : QObject(parent), receiver_(libmueb::MuebReceiver::Instance()) {
+    connect(&receiver_, &libmueb::MuebReceiver::FrameChanged, this,
+            &Task::TestFrame);
+    label_.setFixedSize(320, 260);
+    label_.show();
+  }
+
+ private:
+  libmueb::MuebReceiver &receiver_;
+  QLabel label_;
+
+  void TestFrame(QPixmap frame) { label_.setPixmap(frame.scaled(320, 260)); }
+};
+
+#include "testmuebreceiver.moc"
+
+int main(int argc, char *argv[]) {
+  QApplication a(argc, argv);
+
+  Task task;
+
+  return a.exec();
+}
---
+++ tests/testmuebtransmitter.cc
@@ -0,0 +1,88 @@
+#include <QCoreApplication>
+#include <QObject>
+#include <QThread>
+#include <QTimer>
+
+#include "muebtransmitter.h"
+
+class Task : public QObject {
+  Q_OBJECT
+
+ public:
+  Task(QObject *parent = 0) : QObject(parent) {}
+
+ public slots:
+  void run() {
+    libmueb::MuebTransmitter &transmitter =
+        libmueb::MuebTransmitter::Instance();
+    libmueb::Frame frame = transmitter.frame();
+
+    // Test RGB
+    frame.fill(Qt::red);
+    transmitter.SendFrame(frame);
+    QThread::sleep(1);
+
+    frame.fill(Qt::green);
+    transmitter.SendFrame(frame);
+    QThread::sleep(1);
+
+    frame.fill(Qt::blue);
+    transmitter.SendFrame(frame);
+    QThread::sleep(1);
+
+    frame.fill(Qt::cyan);
+    transmitter.SendFrame(frame);
+    QThread::sleep(1);
+
+    frame.fill(Qt::yellow);
+    transmitter.SendFrame(frame);
+    QThread::sleep(1);
+
+    frame.fill(Qt::magenta);
+    transmitter.SendFrame(frame);
+    QThread::sleep(1);
+
+    // Row test
+    frame.fill(Qt::black);
+    transmitter.SendFrame(frame);
+    for (int row = 0; row < 26; ++row) {
+      frame.fill(Qt::black);
+      transmitter.SendFrame(frame);
+      for (int col = 0; col < 32; ++col) {
+        frame.setPixelColor(col, row, Qt::red);
+      }
+      transmitter.SendFrame(frame);
+      QThread::msleep(250);
+    }
+
+    // Column test
+    frame.fill(Qt::black);
+    transmitter.SendFrame(frame);
+    for (int col = 0; col < 32; ++col) {
+      frame.fill(Qt::black);
+      transmitter.SendFrame(frame);
+      for (int row = 0; row < 26; ++row) {
+        frame.setPixelColor(col, row, Qt::red);
+      }
+      transmitter.SendFrame(frame);
+      QThread::msleep(250);
+    }
+
+    emit finished();
+  }
+
+ signals:
+  void finished();
+};
+
+#include "testmuebtransmitter.moc"
+
+int main(int argc, char *argv[]) {
+  QCoreApplication a(argc, argv);
+
+  Task task;
+  QObject::connect(&task, &Task::finished, &a, &QCoreApplication::quit);
+  QTimer::singleShot(0, &task, &Task::run);
+
+  return a.exec();
+}
